8f1f804263ad0551e63a5cb5f1aa5f21bc1395ea
Pablo Rossa <pablorossa@gmail.com>
2026-06-18 12:06:27 +0200

    r3

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base:"/portfolio2/",
-    base: "./", //al agreagar el . usa rutas relativas  en los enlaces generados por vite
+    //base: "./", //al agreagar el . usa rutas relativas  en los enlaces generados por vite
 });
